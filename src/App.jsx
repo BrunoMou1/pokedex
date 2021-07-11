ea6f131702f8b085aae500ff9cc9241e058ff714
Bruno Moura <brunopmoura1@gmail.com>
2021-07-11 20:32:19 -0300

    tratativa de erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import { useState } from 'react';
+import './styles/global.css'
 import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon] = useState("")
-  const [pokemonData, setPokemonData] = useState([])
-
-  const getPokemon = async () => {
-    const array = []
-    try {
-      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      array.push(res.data)
-      setPokemonData(array)
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  const [pokemonData, setPokemonData] = useState(null)
+  const [error, setError] = useState(null)
 
   const handleChange = (e) => {
     setPokemon(e.target.value)
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    getPokemon()
+    if(!pokemon){
+      return
+    }
+    try {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      setPokemonData(res.data)
+      setError(null)
+    } catch (error) {
+      setError("Pokemon não encontrado")
+      setPokemonData(null)
+    }
   }
 
   return (
-    <div>
+    <div className="container">
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} />
+        <button type='submit'>buscar</button>
       </form>
-      {pokemonData.map(data => (
-        <div>
-          <p>{data.name}</p>
-          <img src={data.sprites.front_default} alt="" />
-        </div>
-      ))}
+      {error && <span>{error}</span>}
+      {pokemonData && (
+        <p>{pokemonData.name}</p>
+      )}
     </div>
   )
 }
